62980a78e82b103e955e620a8534222c07b13cdb
Rahiel <rahiel1205@gmail.com>
2026-09-04 20:36:58 +0200

    Update Password_generator.py

--- Password-Generator/Password_generator.py
+++ Password-Generator/Password_generator.py
@@ -1,13 +1,26 @@
 import secrets #has a stronger randomness compared to the import random. 
 import string 
-
-characters = string.ascii_letters + string.digits + string.punctuation
+characters = ""
 
 password = "" #password is currently empty
 
 length= int(input("how long do you want your password: "))
+print("Y/N answers only.")
+letters = str(input("Do you want letters? "))
+digits = str(input("Do you want digits? "))
+punctuation = str(input("Do you wants punctuation? "))
 
-for i in range(length): 
+if(letters == "Y"): 
+    characters = characters + string.ascii_letters#these are all the letters in lower and upper case 
+    
+if(digits == "Y"): 
+    characters = characters + string.digits
+    
+if(punctuation == "Y"): 
+    characters = characters + string.punctuation
+
+
+for i in range(length): #python for loop  
     password = password + secrets.choice(characters) #the choice will come from characters
 print("Your new password: " + password) 
 
